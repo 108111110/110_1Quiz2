6d62f676f0d9750fe8ae4ad235499d65af052488
誠恩 黃 <108111110@mail.aeust.edu.tw>
2021-11-26 19:24:12 +0800

    [-/]

--- 110_1Quiz2/Reg.aspx.cs
+++ 110_1Quiz2/Reg.aspx.cs
@@ -7,8 +7,9 @@ using System.Web.UI.WebControls;
 
 namespace _110_1Quiz2 {
     public partial class Reg : System.Web.UI.Page {
-        protected void Page_Load(object sender, EventArgs e) {
-
+        protected void Page_Load(object sender, EventArgs e) 
+        {
+            UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
         }
     }
 }
